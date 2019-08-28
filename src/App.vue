743973ff3bfc5a78b4b23bfffc04cc1681149599
h-ARTS <hi5hanan@yahoo.com>
2019-08-28 17:13:16 +0200

    local environment variables and firebase config setup

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,35 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+import Firebase from 'firebase'
+
+
+let config = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: "",
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID
+}
+
+let app = Firebase.initializeApp(config)
+let db = app.database()
+
+let booksRef = db.ref('books')
+
 export default {
   name: 'app',
+  firebase: {
+    books: booksRef
+  },
   components: {
     HelloWorld
-  }
+  },
+  mounted() {
+    console.log(process.env)
+  },
 }
 </script>
 
